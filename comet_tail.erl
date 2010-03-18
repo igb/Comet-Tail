2b84506093e1aa0b07619e85b8d4463d68099f6d
Ian Brown <igb@Ian-Browns-MacBook-Pro.local>
2010-03-17 22:35:36 -0700

    transfer encoding chunked working now...

--- comet_tail.erl
+++ comet_tail.erl
@@ -12,11 +12,17 @@ loop(ListenSock) ->
 	?MODULE:loop(ListenSock).
 	
 handle_request(Sock) ->
-	{ok, Device} = file:open("/tmp/x.txt", [read]),
+	{ok, {http_request, Method, Path, Version}}=gen_tcp:recv(Sock, 0),
+	{abs_path,"/?"++FilePath}=Path,
+	{ok, Device} = file:open(FilePath, [read]),
+	gen_tcp:send(Sock, "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nTransfer-Encoding: chunked\r\n\r\n"),
 	tail_to_socket(Device, Sock).
 
 tail_to_socket(Device, Sock) ->
 	case io:get_line(Device, "") of
         eof  -> tail_to_socket(Device, Sock);
-        Line ->	gen_tcp:send(Sock, Line), tail_to_socket(Device, Sock)
+        Line ->	Length=io_lib:format("~.16B", [length(Line)]),gen_tcp:send(Sock, Length),gen_tcp:send(Sock, "\r\n"),gen_tcp:send(Sock, Line),gen_tcp:send(Sock, "\r\n"), tail_to_socket(Device, Sock)
 	end.
+	
+	
+%%generate_a_1K_comment_for_safari()	
