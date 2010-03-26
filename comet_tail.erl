c1e024958bc195a3735205ab968eea3d1d233c66
Ian Brown <igb@Ian-Browns-MacBook-Pro.local>
2010-03-26 15:18:03 -0700

    pretty much working now...serves up the wrapper page so one can avoid issues with JavaScript security model around remote iframes

--- comet_tail.erl
+++ comet_tail.erl
@@ -1,5 +1,5 @@
 -module(comet_tail).
--export([start/2, loop/2, handle_request/2, is_this_a_safety_path/1, generate_comment_for_length/2]).
+-export([start/2, loop/2, handle_request/2, is_this_a_safety_path/1, generate_comment_for_length/2, generate_iframe_wrapper/1]).
 
 
 start(Port, LogDir)->
@@ -13,12 +13,19 @@ loop(ListenSock, LogDir) ->
 	
 handle_request(Sock, LogDir) ->
 	{ok, {http_request, Method, Path, Version}}=gen_tcp:recv(Sock, 0),
-	{abs_path,"/?"++FilePath}=Path,
-	case (is_this_a_safety_path(FilePath)) of
-		err -> gen_tcp:send(Sock, "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain; charset=UTF-8\r\nConnection: close\r\n\r\nInvalid File Path\r\n\r\n"),gen_tcp:close(Sock);
-		ok -> {ok, Device} = file:open(lists:append(LogDir,FilePath), [read]), 
-		gen_tcp:send(Sock, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nCache-Control: no-cache\r\nTransfer-Encoding: chunked\r\n\r\n"),
-		tail_to_socket(Device, Sock)
+	case (Path) of 
+		{abs_path,"/?"++FilePath}->
+			case (is_this_a_safety_path(FilePath)) of
+				err -> gen_tcp:send(Sock, "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain; charset=UTF-8\r\nConnection: close\r\n\r\nInvalid File Path\r\n\r\n"),gen_tcp:close(Sock);
+				ok -> {ok, Device} = file:open(lists:append(LogDir,FilePath), [read]), 
+				gen_tcp:send(Sock, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nCache-Control: no-cache\r\nTransfer-Encoding: chunked\r\n\r\n"),
+				tail_to_socket(Device, Sock)
+			end;
+		{abs_path,"/comet-tail?"++FilePath} ->
+				HtmlFile=generate_iframe_wrapper(FilePath),
+				HtmlLength=lists:flatten(io_lib:format("~p", [length(HtmlFile)])),
+				gen_tcp:send(Sock, lists:append(["HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html; charset=UTF-8\r\nCache-Control: no-cache\r\nContent-Length: ", HtmlLength, "\r\n\r\n", HtmlFile])),
+				gen_tcp:close(Sock)
 	end.
 
 tail_to_socket(Device, Sock) ->
@@ -51,4 +58,8 @@ generate_comment_for_length(Comment, 0) ->
 generate_comment_for_length(Comment, Length) ->
 	generate_comment_for_length(lists:append(Comment, " "), Length -1).
 		
+%% generate a iframe wrapper
+generate_iframe_wrapper(FilePath)->
+	lists:append(["<html><body><iframe id='logframe' frameborder='0' src='/?", FilePath, "'    width='100%' height='95%' scrolling='no'> </iframe><script>function loop() { if(navigator.userAgent.indexOf('Safari') > 0) { 	window.frames['logframe'].scrollBy(0 , 10000000); } else  { document.getElementById('logframe').contentWindow.scrollBy(0, 100000); } setTimeout('loop()', 1000); } loop(); </script> </body></html>"]).	
+
 			
